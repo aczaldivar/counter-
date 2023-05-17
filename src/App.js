c3b987374538a196a3a3a0a51fa43b4ccd4cd1ff
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-05-17 11:13:01 -0700

    counter twice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [counter, changeCounter ] = useState(1)
+ return (
+    <div>
+      <h1>
+        Counter
+      </h1>
+      <p>
+        {counter}
+      </p>
+      <p>
+        {counter}
+      </p>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App 
